072527cd8601142e14e244bf001b734c813796e7
joserosalesdh <jm.rosales92@gmail.com>
2020-10-19 16:16:43 -0300

    comit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { componentFactoryName } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-header',
+    template: `<h1>Header component</h1>`
+})
+export class HeaderComponent {
+
+}
